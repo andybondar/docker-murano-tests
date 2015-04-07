829cd7db895274739421657e6ec25042abe17208
Andriy Bondar <abondar@mirantis.com>
2015-04-07 15:48:35 +0300

    Hotfix in 'run_test_deploy_docker_advanced.sh'

--- run_test_deploy_docker_advanced.sh
+++ run_test_deploy_docker_advanced.sh
@@ -16,8 +16,8 @@ do
     nosetests -sv test_deploy_docker_advanced.py:MuranoDockerTestAdvanced.$app_test &> logs/advanced_${app_test}.log
     result=`cat logs/${app_test}.log | head -1 | awk '{print $3}'`
     if [ "$result"="ok" ]; then
-	mv logs/${app_test}.log logs/advanced_${app_test}_OK.log
+	mv logs/advanced_${app_test}.log logs/advanced_${app_test}_OK.log
     else
-	mv logs/${app_test}.log logs/advanced_${app_test}_${result}.log
+	mv logs/advanced_${app_test}.log logs/advanced_${app_test}_${result}.log
     fi
 done
