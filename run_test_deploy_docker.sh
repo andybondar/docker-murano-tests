18009e4c007692a3b18f636a2914814a68f2f8ac
Andriy Bondar <abondar@mirantis.com>
2015-04-19 18:57:38 +0300

    Modified log parsing one more time

--- run_test_deploy_docker.sh
+++ run_test_deploy_docker.sh
@@ -15,11 +15,13 @@ for i in "${array[@]}"
 do
     app_test=`python deploy_docker_list.py $i`
     nosetests -sv test_deploy_docker.py:MuranoDockerTest.$app_test &> logs/${app_test}.log
-    result=`head -1 logs/${app_test}.log | awk '{print $3}'`
-    if [ "$result" = "ok" ]; then
-	mv logs/${app_test}.log logs/${app_test}_OK.log
-    else
-	mv logs/${app_test}.log logs/${app_test}_${result}.log
-    fi
+#    result=`head -1 logs/${app_test}.log | awk '{print $3}'`
+    result=`tail -1 logs/${app_test}.log | awk '{print $1}'`
+    tail -3 logs/${app_test}.log | head -1
+#    if [ "$result" = "ok" ]; then
+#	mv logs/${app_test}.log logs/${app_test}_OK.log
+#    else
+    mv logs/${app_test}.log logs/${app_test}_${result}.log
+#    fi
 
 done
