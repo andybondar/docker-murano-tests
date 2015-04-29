0a988797dc1413667d0d65bc503be5a3224c7e12
Andriy Bondar <abondar@mirantis.com>
2015-04-29 11:10:18 +0300

    Remove 'OK' logs before exit

--- handle_results.sh
+++ handle_results.sh
@@ -5,6 +5,8 @@ errors=`ls -lh logs/ | grep ERROR | wc -l`
 
 sum=$(($fails + $errors))
 
+rm -f logs/*_OK.log
+
 if [ "$sum" -gt 0 ]; then
     echo "There are $fails fails and $errors errors..."
     exit 1
@@ -14,4 +16,3 @@ if [ "$sum" -gt 0 ]; then
     echo "All tests passed" > logs/results.log
 fi
 
-rm -f logs/*_OK.log
